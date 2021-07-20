4b2fcbfd13a07b934e6ce71979218ac8db8a30d0
Klimihyeon <lordnayo@gmail.com>
2021-07-20 18:12:22 +0900

    go home

--- docs/test/src/main/java/com/spring/controller/QRMaker.java
+++ docs/test/src/main/java/com/spring/controller/QRMaker.java
@@ -21,7 +21,7 @@ public class QRMaker {
                 file.mkdirs();
             }
             // 코드인식시 링크걸 URL주소
-            String codeurl = new String("hwiro".getBytes("UTF-8"), "ISO-8859-1");
+            String codeurl = new String("momo".getBytes("UTF-8"), "ISO-8859-1");
             // 큐알코드 바코드 생상값
             int qrcodeColor =   0xff44b316;
             // 큐알코드 배경색상값
